0a84d40d05307e6fc27bc57c282b1e693302e2cf
Pov4rmank <jokersvekla@gmail.com>
2015-01-06 22:57:30 +0900

    звук стрельбы , какая то пуля я хуй знает как её вставить

---
+++ Assets/Scripts/Shooting.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Shooting : MonoBehaviour {
+	AudioSource  Audio;
+	float reloadprogress;
+	int reloadtime= 1;
+	bool isReloading;
+	// Use this for initialization
+	void Start () {
+		Audio= GetComponent <AudioSource> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (isReloading) reload();
+
+		if (Input.GetMouseButtonDown (0) && !isReloading)
+			shoot();
+	}
+	void shoot()
+	{
+		Audio.Play ();
+		reloadprogress = reloadtime;
+		isReloading = true;
+	}
+	void reload()
+	{
+		reloadprogress -=Time.deltaTime;
+		if (reloadprogress<0) isReloading = false;
+	}
+}
