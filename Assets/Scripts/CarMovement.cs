68cf3c843b52a82e149de39e478d1ff281fb82c2
Alexey <agoryachev89@gmail.com>
2015-01-13 23:25:39 +0900

    Добавил машину и управление камерой

---
+++ Assets/Scripts/CarMovement.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CarMovement : MonoBehaviour {
+	public float speed;
+	public GameObject player;
+	public GameObject driverPosition;
+	Vector3 movement;
+	Vector3 driverPos;
+	Rigidbody carRigidbody;
+	Rigidbody playerRigidbody;
+
+	void Awake ()
+	{
+		carRigidbody = GetComponent <Rigidbody> ();
+		playerRigidbody = player.GetComponent <Rigidbody> ();
+		movement.Set (1f, 0f, 0f);
+		driverPos.Set (driverPosition.transform.position.x - this.transform.position.x,driverPosition.transform.position.y - this.transform.position.y,driverPosition.transform.position.z - this.transform.position.z);
+	}
+	
+	void Update () {
+		movement.Set (1f, 0f, 0f);
+		movement = movement * speed * Time.deltaTime;
+		carRigidbody.MovePosition(transform.position + movement);
+		playerRigidbody.MovePosition(transform.position + movement + driverPos);
+	}
+}
