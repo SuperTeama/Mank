c8bd591178ae4cd00637145a1cda24025ae2c02b
Pov4rmank <jokersvekla@gmail.com>
2015-01-31 19:20:46 +0900

    new landplacement sys

---
+++ Assets/Scripts/GOOwner.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+public class GOOwner : MonoBehaviour {
+	public GameObject[] objectList;
+	public string path;
+	public GameObject[] GetGOList()
+	{
+		return objectList;
+	}
+
+	public void PlaceNextLand()
+	{
+		float offset = 0;
+		foreach (GameObject obj in objectList)
+			offset += GetSpriteWidth (obj);
+		Vector3 pos = objectList [0].transform.position;
+		pos.x += offset;
+		objectList[0].transform.position = pos;
+		objectList[0].GetComponent<SpriteRenderer> ().sprite =GetRandomTexture();
+		ShuffleList();
+	}
+	private void ShuffleList()
+	{
+		GameObject temp;
+		for (int i=0; i<objectList.Length-1; i++) 
+		{
+			temp=objectList[i+1];
+			objectList[i+1]=objectList[i];
+			objectList[i]=temp;
+		}
+	}
+	public void CheckThisPosition(float Xpos)
+	{
+		if ( Xpos > objectList[1].transform.position.x)	PlaceNextLand ();
+	}
+	private Sprite GetRandomTexture()
+	{
+		Sprite[] spriteList = Resources.LoadAll<Sprite> (path);
+		int someRandomShit = Random.Range(0, spriteList.Length); 
+		return spriteList[someRandomShit]; 
+	}
+	private float GetSpriteWidth(GameObject land)
+	{
+		return land.GetComponent<SpriteRenderer>().sprite.bounds.max.x - land.GetComponent<SpriteRenderer>().sprite.bounds.min.x;
+	}
+}
--- Assets/Scripts/LandPlacement.cs
+++ Assets/Scripts/LandPlacement.cs
@@ -1,51 +1,18 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEditor;
+using System.Collections.Generic;
 
 public class LandPlacement : MonoBehaviour {
-	public GameObject[] roadList;
+	public GOOwner[] scheme;
 	public GameObject car;
-	private Vector3 landNewPosition = new Vector3(0,0,0);
 	public static LandPlacement Inst = new LandPlacement();
-	private Sprite[] landSprites;
-	void Start () {
-		landSprites = Resources.LoadAll<Sprite>("Images/Lands");
-	}
-	private void ShuffleList(GameObject[] list)
-	{
-		GameObject temp;
-		for (int i=0; i<list.Length-1; i++) 
-		{
-			temp=list[i+1];
-			list[i+1]=list[i];
-			list[i]=temp;
-		}
-	}
-	private void PlaceNextLand(GameObject[] list)
-	{
-		float offset = 0;
-		foreach (GameObject obj in list)
-			offset += GetSpriteWidth (obj);
 
-		float pos = list[0].transform.position.x;
-		landNewPosition.x = (pos + offset);
-		list[0].transform.position =landNewPosition;
-		list[0].GetComponent<SpriteRenderer> ().sprite =GetRandomTexture();
-		ShuffleList(list);
-	}
-	private Sprite GetRandomTexture()
-	{
-		int someRandomShit = Random.Range(0, landSprites.Length); 
-		return landSprites[someRandomShit]; 
-	}
+	void Start () {}
 
-	private float GetSpriteWidth(GameObject land)
-	{
-		return land.GetComponent<SpriteRenderer>().sprite.bounds.max.x - land.GetComponent<SpriteRenderer>().sprite.bounds.min.x;
-	}
 	void Update() 
 	{
-		if (car.transform.position.x > roadList[1].transform.position.x)
-			PlaceNextLand (roadList);
+		foreach ( GOOwner govner in scheme)
+			govner.CheckThisPosition(car.transform.position.x);
 	}
 }
