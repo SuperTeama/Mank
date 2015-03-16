93d5566d255e350a19c7133ba53d0f9c63e7e961
Pov4rmank <jokersvekla@gmail.com>
2015-03-17 00:22:02 +0900

    NEW GAME

---
+++ Assets/Scripts/player/playercontrol.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class playercontrol : MonoBehaviour {
+	float maxspeed;
+	float curMoveSpeed;
+	float accelerationTime;
+	bool moving = false;
+	Vector2 nullVector = new Vector2(0f,0f);
+	Vector2 dir;
+	void Start () {
+		dir = nullVector;
+		maxspeed= 0.1f;
+		curMoveSpeed = 0.0f;
+		accelerationTime =0.2f;
+	}
+	
+	void Update ()
+	{
+		dir = GetDirection();
+		moving = (dir != nullVector);
+
+		if (moving)
+		{
+			curMoveSpeed += maxspeed * (Time.deltaTime / accelerationTime);
+			curMoveSpeed = Math.Min (maxspeed, curMoveSpeed);
+		}
+		else 
+			curMoveSpeed = 0;
+
+		CalcMoveSpeed ();
+
+		GetComponent<Transform>().Translate(dir*curMoveSpeed);
+		transform.Rotate (Input.mousePosition); // хуйня какая то
+	}
+	void  CalcMoveSpeed()
+	{
+
+	}
+	Vector2 GetDirection()
+	{
+		Vector2 res = nullVector;
+		if (Input.GetKey (KeyCode.D)) {
+			res.x+=1f;
+		}
+		if (Input.GetKey (KeyCode.A)) {
+			res.x-=1f;
+		}
+		if (Input.GetKey (KeyCode.W)) {
+			res.y+=1f;
+		}
+		if (Input.GetKey (KeyCode.S)) {
+			res.y-=1f;
+		}
+		return res.normalized;
+	}
+	
+}
